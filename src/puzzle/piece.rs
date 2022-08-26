8d961e1fb47cde3fbe093c2bb43a3888d7ba2065
Rowan Clarke <r.n.clarke@lancaster.ac.uk>
2022-08-26 21:40:23 +0100

    Removed Piece Struct

--- src/puzzle/piece.rs
+++ src/puzzle/piece.rs
@@ -1,5 +1,3 @@
-use std::{iter, slice};
-
 #[derive(Copy, Clone)]
 pub enum Link {
     None,
@@ -12,17 +10,103 @@ pub enum Link {
 #[derive(Copy, Clone)]
 pub struct Side(pub Link, pub Link);
 
-#[derive(Copy, Clone)]
-pub struct Piece {
-    sides: [Side; 4],
-}
+pub type Piece = [Side; 4];
 
-impl Piece {
-    pub fn new(sides: [Side; 4]) -> Self {
-        Self { sides }
-    }
-
-    pub fn sides(&self) -> &[Side] {
-        self.sides.as_ref()
-    }
-}
+pub const pieces: [Piece; 16] = [
+    [
+        Side(Link::Track, Link::Road),
+        Side(Link::River, Link::Track),
+        Side(Link::River, Link::None),
+        Side(Link::Track, Link::Road),
+    ],
+    [
+        Side(Link::River, Link::Track),
+        Side(Link::Path, Link::Track),
+        Side(Link::River, Link::Road),
+        Side(Link::Road, Link::Path),
+    ],
+    [
+        Side(Link::Road, Link::Path),
+        Side(Link::River, Link::Path),
+        Side(Link::Road, Link::None),
+        Side(Link::River, Link::Path),
+    ],
+    [
+        Side(Link::Road, Link::Track),
+        Side(Link::Track, Link::River),
+        Side(Link::Path, Link::Track),
+        Side(Link::Road, Link::River),
+    ],
+    [
+        Side(Link::Path, Link::River),
+        Side(Link::None, Link::Road),
+        Side(Link::Path, Link::River),
+        Side(Link::None, Link::Road),
+    ],
+    [
+        Side(Link::Road, Link::Track),
+        Side(Link::Road, Link::None),
+        Side(Link::Track, Link::Path),
+        Side(Link::River, Link::River),
+    ],
+    [
+        Side(Link::Track, Link::Path),
+        Side(Link::Path, Link::River),
+        Side(Link::River, Link::Road),
+        Side(Link::Road, Link::Track),
+    ],
+    [
+        Side(Link::Road, Link::River),
+        Side(Link::Path, Link::Track),
+        Side(Link::Track, Link::Road),
+        Side(Link::River, Link::Track),
+    ],
+    [
+        Side(Link::None, Link::Track),
+        Side(Link::None, Link::Track),
+        Side(Link::Track, Link::River),
+        Side(Link::River, Link::Track),
+    ],
+    [
+        Side(Link::River, Link::Road),
+        Side(Link::Path, Link::River),
+        Side(Link::Road, Link::River),
+        Side(Link::None, Link::River),
+    ],
+    [
+        Side(Link::Track, Link::Road),
+        Side(Link::Path, Link::Road),
+        Side(Link::Track, Link::River),
+        Side(Link::River, Link::Path),
+    ],
+    [
+        Side(Link::Track, Link::Path),
+        Side(Link::River, Link::Road),
+        Side(Link::River, Link::Path),
+        Side(Link::Road, Link::Track),
+    ],
+    [
+        Side(Link::River, Link::Track),
+        Side(Link::Path, Link::River),
+        Side(Link::Track, Link::Path),
+        Side(Link::River, Link::River),
+    ],
+    [
+        Side(Link::River, Link::Path),
+        Side(Link::Track, Link::Path),
+        Side(Link::None, Link::Road),
+        Side(Link::Road, Link::River),
+    ],
+    [
+        Side(Link::Path, Link::Track),
+        Side(Link::Road, Link::River),
+        Side(Link::Track, Link::None),
+        Side(Link::Road, Link::River),
+    ],
+    [
+        Side(Link::Track, Link::River),
+        Side(Link::River, Link::Road),
+        Side(Link::Road, Link::River),
+        Side(Link::River, Link::Path),
+    ],
+];
