27dc4d7b3515c35893f507d2e65dccf026cb7cb5
Rowan Clarke <r.n.clarke@lancaster.ac.uk>
2022-08-29 21:38:44 +0100

    Solved Puzzle

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,12 @@
 pub mod puzzle;
 pub mod utils;
 
+use puzzle::piece::*;
 use puzzle::*;
+use utils::cartesian::*;
 
-fn main() {}
+fn main() {
+    let mut puzzle = Puzzle::new();
+    let result = puzzle.place_all(puzzle.get_least().unwrap());
+    println!("{:?}", result.ok());
+}
--- src/puzzle.rs
+++ src/puzzle.rs
@@ -1,66 +1,114 @@
-mod piece;
+pub mod piece;
 
 use crate::utils::cartesian::Cartesian;
 use piece::*;
 
 pub struct Puzzle {
-    possible: [u64; 16],
+    pub possible: [u64; 16],
+    pub visited: u16,
 }
 
 impl Puzzle {
-    fn get(&self, (x, y): (usize, usize)) -> u64 {
-        self.possible[(x * 4) + y]
+    fn get(&self, (x, y): (usize, usize)) -> Option<u64> {
+        if x > 3 || y > 3 {
+            return None;
+        }
+        Some(self.possible[(x * 4) + y])
     }
 
     fn mask(&mut self, (x, y): (usize, usize), m: u64) {
         self.possible[(x * 4) + y] &= m;
     }
 
-    fn set(&mut self, (x, y): (usize, usize), (p, r): (usize, usize)) {
-        // Set piece
-        self.mask((x, y), 1 << ((p * 4) + r));
-
+    pub fn set(&mut self, (x, y): (usize, usize), (p, r): (usize, usize)) {
         // Take piece from all possibilities
         for xy in Cartesian::new(0..4, 0..4) {
             self.mask(xy, !(15 << (p * 4)));
         }
+        // Set and visit piece
+        self.possible[(x * 4) + y] = 1 << ((p * 4) + r);
+        self.visited |= 1 << ((x * 4) + y);
 
         // Remove unmatching from neighbours
-        for (i, xy) in [(x, y + 1), (x + 1, y), (x, y - 1), (x - 1, y)]
-            .into_iter()
-            .enumerate()
-        {
+        for (i, xy) in [
+            (x.checked_add(1), Some(y)),
+            (Some(x), y.checked_add(1)),
+            (x.checked_sub(1), Some(y)),
+            (Some(x), y.checked_sub(1)),
+        ]
+        .into_iter()
+        .enumerate()
+        .filter_map(|(i, xy)| match xy {
+            (Some(x), Some(y)) => Some((i, (x, y))),
+            _ => None,
+        }) {
             // Side to match
-            let side = pieces[p][i];
+            let side = PIECES[p][(r + i) % 4];
             // Go through all possibilities
-            let current = self.get(xy);
-            for (po, ro) in Cartesian::new(0..16, 0..4).filter(|&(po, ro)| {
-                (1 << ((po * 4) + ro)) & current != 0 && pieces[po][(ro + 2) % 4] != side
-            }) {
-                // Remove unmatching sides
-                self.mask(xy, !(1 << ((po * 4) + ro)));
+            if let Some(current) = self.get(xy) {
+                for (po, ro) in Cartesian::new(0..16, 0..4).filter(|&(po, ro)| {
+                    (1 << ((po * 4) + ro)) & current != 0 && PIECES[po][(ro + i + 2) % 4] != side
+                }) {
+                    // Remove unmatching sides
+                    self.mask(xy, !(1 << ((po * 4) + ro)));
+                }
             }
         }
     }
 
-    pub fn place_all(&mut self, (x, y): (usize, usize)) {
-        let state = self.possible;
+    // Get the (x, y) of the piece with the most constrait for optimisation
+    pub fn get_least(&self) -> Option<(usize, usize)> {
+        Cartesian::new(0..4, 0..4)
+            .filter(|&(x, y)| self.visited & (1 << ((x * 4) + y)) == 0)
+            .min_by_key(|&xy| {
+                (0..64)
+                    .map(|m| (1 << m) & self.get(xy).unwrap())
+                    .filter(|&x| x != 0)
+                    .count()
+            })
+    }
+
+    // TODO Replace with place_next(&mut self)
+    pub fn place_all(
+        &mut self,
+        (x, y): (usize, usize),
+    ) -> Result<Vec<((usize, usize), (usize, usize))>, NoSolutionError> {
+        let state = (self.possible, self.visited);
         // Go through all possibilities
-        let current = self.get((x, y));
+        let current = self.get((x, y)).unwrap();
         for (p, r) in
             Cartesian::new(0..16, 0..4).filter(|(p, r)| (1 << ((p * 4) + r)) & current != 0)
         {
-            // Set possibility
+            // Set piece
             self.set((x, y), (p, r));
-            // TODO place next piece
-            // Undo changes
-            self.possible = state;
+            if let Some(least) = self.get_least() {
+                // Place next piece;
+                match self.place_all(least) {
+                    Ok(mut vec) => {
+                        // Push solution
+                        vec.push(((x, y), (p, r)));
+                        return Ok(vec);
+                    }
+                    Err(_) => {
+                        // Undo changes
+                        (self.possible, self.visited) = state;
+                    }
+                }
+            } else {
+                let mut vec = Vec::with_capacity(16);
+                vec.push(((x, y), (p, r)));
+                return Ok(vec);
+            }
         }
+        return Err(NoSolutionError);
     }
 
     pub fn new() -> Self {
         Self {
             possible: [u64::MAX; 16],
+            visited: 0,
         }
     }
 }
+
+pub struct NoSolutionError;
